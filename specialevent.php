56d9ea9dff3e28761200c7b715135ec59c35ae40
sheila honey <sheila_honey_5@hotmail.com>
2024-04-24 08:02:19 -0700

    remove sale stuff on cruise

--- specialevent.php
+++ specialevent.php
@@ -26,23 +26,22 @@
             src="img/cruise.png" alt="cruise"></h1>
  
   
-    <h2>Cruise Planners are providing a deal on a New Year's Dance cruise.</h2>
 
-    <h3> You can join some of your fellow club members and
-    take a memorable journey that goes beyond the
-destinations visited aboard our iconic ships — Queen Elizabeth®</h3>
-<h3>
-<em>For a limited time</em>, take advantage of Cunard’s latest sales coupled with our Cruise Planners CRUISE SALE 4/16-4/23 for the best current rates, on board credits and reduced deposits.  
+
+   
+    <h3>
+     To date (April 2024), approximately 40 SaddleBrooke Ballroom Dance Club members have booked staterooms on this cruise.  
+    Club members interested in joining the fun and going on the cruise can access detailed information using the links below or Contact Susan or Jerry at Cruise Planners.  
     </h3>
-    
+    <h3>
     <ul>
-          <li class="li-none"><a href="https://drive.google.com/file/d/1P08KiOFzkvCw-d8Uqxn_DL6L5ic-qV1w/view?usp=sharing">Click to get more information about the cruise sale.</a></li>
-          <li class="li-none"><a href="https://drive.google.com/file/d/1Jt3-0jWUezLERNlI8wUs4AgRtko4PV1c/view?usp=drive_link">Click to get the deck plans of the QE2</a></li>
+
           <li class="li-none"><a href="https://www.cruiseplannersandmore.com/cs/1712274">Click to see the Dance Cruise details</a></li>
     </ul>
+    </h3>
     <br>
 
-    <h3>If you are not ready to book - no worries - we will have similar sales in future months, although rates are subject to change.</h3><br>
+
     <h4>Brought to you by</h4>
     <h4>Susan & Jerry Wilson<br>
     Click below to email.</h4>
@@ -51,10 +50,9 @@ destinations visited aboard our iconic ships — Queen Elizabeth®</h3>
     susan.wilson@cruiseplanners.com</a> / 
     <a href="mailto:adinolfir@outlook.com?subject=SBDC Info">
     jerry.wilson@cruiseplanners.com</a>
-    </h3>
-<h4>
-(520) 314-0214</h4>
-    </div>
+    
+<br>
+(520) 314-0214 </h3>
 <?php
   include 'footer.php';
 ?>
