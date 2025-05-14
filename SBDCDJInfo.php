5f2b4af33afca2656468fa27ff8509d9de2571a8
sheila honey <sheila_honey_5@hotmail.com>
2025-05-14 15:39:15 -0700

    revamp dj page

--- SBDCDJInfo.php
+++ SBDCDJInfo.php
@@ -30,18 +30,19 @@ require_once 'includes/siteemails.php';
            and quite a large 
            body of club music available. 
            We even have some pre-made song lists to facilitate
-           DJ duties. See below for all the documents we've put together. </p><br>
-        <p>
-            Also, Check out the Automated DJ APP (ROBO DJ) that can be used where WIFI is available
-        </p><br>
+           DJ duties. See below for all the documents we've put together. </p>
+
        <a  target="_blank"
 href="https://drive.google.com/drive/folders/1LjnghlW8uftZHNxDG1YN4hbkq5AU2f7f?usp=sharing">
 DJ Documents</a><br>
-        <a  target="_blank" href="https://sbdcrobodj.com/">Go to ROBO DJ App</a><br>
-           <a target="_blank" href="https://drive.google.com/file/d/1lRa1Sr_RIpyj-yLF_9qeZgdC7QGZ5q-X/view?usp=sharing"><em>Click for ROBO DJ Guide</em></a><br>
-        <a  target="_blank" href="https://zedar.com?x=82351 "
-        >DJ Equipment Checkout App</a><br>
-        <a target="_blank" href="https://drive.google.com/file/d/1y6GRdngzNWCgx-xSKYwk2wtc-8qK8eGC/view?usp=sharing">DJ Policy</a><br>
+     <a target="_blank" href="https://drive.google.com/file/d/1y6GRdngzNWCgx-xSKYwk2wtc-8qK8eGC/view?usp=sharing">DJ Policy</a><br><br>
+             <p>
+            Also, Check out the Automated DJ APP (ROBO DJ) that can be used where WIFI is available
+        </p>
+       
+        <a target="_blank" href="https://drive.google.com/file/d/1lRa1Sr_RIpyj-yLF_9qeZgdC7QGZ5q-X/view?usp=sharing"><em>Click for ROBO DJ Guide</em></a><br>
+         <a  target="_blank" href="https://sbdcrobodj.com/">Go to ROBO DJ App</a><br>
+   
         <?php
         echo '<p>Contact our DJ coordinator,<a href="mailto:'.$djDirector.'?subject=SBDC DJ Info">';
         ?>
