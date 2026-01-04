376381b234ba2ea25da4059bb95a488d61acd958
sheila honey <sheila_honey_5@hotmail.com>
2026-01-04 07:30:20 -0700

    add nov 2025 pictures

--- SBDCPictures.php
+++ SBDCPictures.php
@@ -62,6 +62,7 @@ $sess = session_start();
        <li class="li-none"><a href="https://sheilahoney.smugmug.com/SBDC-Lazy-Daze-of-Summer-08-21-2025">Lazy Daze of Summer 08 21 2025</a></li>
        <li class="li-none"><a href="https://sheilahoney.smugmug.com/SBDC-October-2025-Dance-Party">October Dance Party 10 15 2025</a></li>
        <li class="li-none"><a href="https://sheilahoney.smugmug.com/SBDC-Charity-Walkers-2025">Charity Walk 11 08 2025</a></li>
+       <li class="li-none"><a href="https://sheilahoney.smugmug.com/SBDC-November-2025-Luau">November Luau 11 14 2025</a></li>
         <li class="li-none"><a href=" https://sheilahoney.smugmug.com/SBDC-December-2025-Holiday-Gala">Holiday Gala 12 12 2025</a></li>
       
        
